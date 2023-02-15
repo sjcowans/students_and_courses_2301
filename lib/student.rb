35ed276cfad97bc7c557ffa16cb7aac7bd1bd0b3
Sean Cowans <zedesia@yahoo.com>
2023-02-15 09:21:02 -0700

    Create student file

--- lib/student.rb
+++ lib/student.rb
@@ -1,1 +1,16 @@
+class Student
+  attr_reader :name, :scores, :age
+  def initialize(student_info)
+    @name = student_info[:name]
+    @age = student_info[:age]
+    @scores = []
+  end
 
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    (scores.sum).div(scores.length)
+  end
+end
