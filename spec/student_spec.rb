86ef8f1e1e835c7858d03b0b1f17430e652bc296
Sean Cowans <zedesia@yahoo.com>
2023-02-15 09:20:47 -0700

    Create student spec

---
+++ spec/student_spec.rb
@@ -0,0 +1,33 @@
+require './lib/student'
+
+RSpec.describe Student do
+  it 'exists' do
+    student = Student.new({name: "Morgan", age: 21})
+
+    expect(student).to be_a(Student)
+  end
+
+  it 'has proper attributes' do
+    student = Student.new({name: "Morgan", age: 21})
+
+    expect(student.name).to eq("Morgan")
+    expect(student.age).to eq(21)
+    expect(student.scores).to eq([])
+  end
+
+  it 'can log scores' do
+    student = Student.new({name: "Morgan", age: 21})
+    student.log_score(89)
+    student.log_score(78)
+
+    expect(student.scores).to eq([89, 78])
+  end
+
+  it 'can average the grades' do
+    student = Student.new({name: "Morgan", age: 21})
+    student.log_score(89)
+    student.log_score(78)
+
+    expect(student.grade).to eq(83.5)
+  end
+end
